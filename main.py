e5f0dbdde40e5a918987b2a34a3c202aed12182c
FreakyAvi <aviralgupta010704@gmail.com>
2024-06-29 04:23:50 +0530

    Added support for CSV file

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
-import pandas
+import pandas as pd
 import numpy
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from bs4 import BeautifulSoup
 import torch
 import re
 
+def SApipeline(text):
+    #creating tokens
+    tokens = tokenizer.encode(text, return_tensors='pt')
+    #print(tokens)       # check the tokens
+
+    #pass tokens to model
+    results = model(tokens)
+    #print(results)  #check results
+
+    #print(f"Logits: {results.logits}") # Check logits
+
+    SAscore = int(torch.argmax(results.logits)) + 1
+    #print(f"Sentiment Score:{SAscore}")
+    return(SAscore)
+
 #initializing model and tokenizer
 tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
 model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+t = "it is okay"
+# SApipeline(t)
 
-#creating tokens
-tokens = tokenizer.encode('THIS IS THE BEST THING THAT HAS ECER HAPPENED TO ME THANK YOU SO MUCH!', return_tensors='pt')
-print(tokens)       # check the tokens
+df = pd.read_csv("AmazonProdReviews.csv")
+# print(df.head())
 
-#pass tokens to model
-results = model(tokens)
-print(results)  #check results
+if "SA Score" not in df.columns.tolist():
+    df["SA Score"] = None
 
-print(f"Logits: {results.logits}") # Check logits
+for i in range(len(df)):
+    if df.loc[i, "SA Score"] not in [1,2,3,4,5]:
+        df.loc[i, "SA Score"] = SApipeline(df.loc[i, "reviews"])
 
-SAscore = int(torch.argmax(results.logits)) + 1
-print(f"Sentiment Score:{SAscore}")
+df.to_csv("AmazonProdReviews.csv", index=False)
+print("PROCESS COMPLETED!")
+print(df.tail())
+
+
+
+
+
